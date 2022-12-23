f4958a3dd9c11d951b292dd961b6c6b57933e502
Saheli Das <sahelidas@Sahelis-MacBook-Pro.local>
2022-12-23 17:01:12 +0530

    QR code endpoint & update postman collection

--- MetroBot/models/paymentModel.js
+++ MetroBot/models/paymentModel.js
@@ -1,19 +1,23 @@
 /* eslint-disable no-undef */
 const { Schema } = mongoose = require('mongoose')
+var uuid = require('node-uuid')
+const uuidv4= require('uuid');
+
 
 const ticketSchema = new Schema({
-  ticketId: Number,
+  ticketId: String,
   ticketQR: String,
   ticketStatus: String
 }, { timestamps: true })
 
 const paymentSchema = new Schema({
-  transactionId: { type: String, unique: true },
+  transactionId: { type: String, default: uuidv4.v4(), required: true },
   transactionAmount: Number,
   ticketType: String,
   numberOfTickets: Number,
-  sourceStation: String,
-  destinationStation: String,
+  sourceStationCode: String,
+  destinationStationCode: String,
+  sessionId : { type: String, required: true},
   tickets: [ticketSchema]
 }, { timestamps: true },
 { collection: 'payments' })
--- MetroBot/routes/paymentRoutes.js
+++ MetroBot/routes/paymentRoutes.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const Payment = require('../models/paymentModel')
+const QRCode = require('qrcode')
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+const ticketPDFFilePath = process.env.TICKET_PDF_FILE_PATH 
 
 // Create a new Payment
 router.post('/', async (req, res) => {
   const payment = new Payment({
     transactionId: req.body.transactionId,
+    sessionId:req.body.sessionId,
     transactionAmount: req.body.transactionAmount,
     ticketType: req.body.ticketType,
     numberOfTickets: req.body.numberOfTickets,
-    sourceStation: req.body.sourceStation,
-    destinationStation: req.body.destinationStation,
+    sourceStationCode: req.body.sourceStationCode,
+    destinationStationCode: req.body.destinationStationCode,
     tickets: req.body.tickets
   })
 
@@ -42,5 +48,74 @@ router.get('/:paymentId', async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 })
+
+// Get the payment with paymentId
+// for each ticket generate the QR code and save the pdf
+// Build the response with the ticket pdf details
+
+// QR code Method
+router.get('/QRCode/:paymentId', async (req, res) => {
+  // Get by the payment id provided in the request
+  try {
+    const payment = await Payment.findOne({ paymentId: req.params.paymentId })
+    
+    const pdfs= await generateQRCodeImage(payment)
+  res.send(pdfs)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+
+//Generate QR code with pdf
+async function generateQRCodeImage(payment){
+  const tickets = payment.tickets
+  const transactionId = payment.transactionId
+  const sourceStationCode =payment.sourceStationCode
+  const destinationStationCode= payment. destinationStationCode
+  const transactionAmount = payment.transactionAmount
+  const transactionTime =payment.createdAt
+  let QRcodeInfo ={transactionId, transactionAmount, sourceStationCode,destinationStationCode,transactionTime}
+  let pdfFilePath=[]
+    for(let i=0; i< tickets.length; i++) {
+      let ticketId = tickets[i].ticketId 
+      let ticketStatus =tickets[i].ticketStatus
+      QRcodeInfo.ticketId =  ticketId
+      QRcodeInfo.ticketStatus =ticketStatus
+      //Convert the data into String format
+      let QRcodeInfoString= JSON.stringify(QRcodeInfo)
+      //Generate QR code
+      await QRCode.toFile(
+        `QRimage/${ticketId}.png`, QRcodeInfoString,
+        [{
+            data: [10, 10, 10],
+            mode: 'byte'
+        }])
+    //Generate pdf    
+      await pdfMaker(ticketId)
+      pdfFilePath.push(`${ticketPDFFilePath}/${ticketId}.pdf`)
+  };
+  return pdfFilePath
+}
+
+//Genarate pdf
+async function pdfMaker(ticketId) {
+   
+  // Create a document
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream(`${ticketPDFFilePath}/${ticketId}.pdf`));
+  doc
+      .fontSize(25)
+      .text(`Ticket number: ${ticketId}`, 100, 100)
+      .align ? 'center' : 'center'
+
+  // Add an image, constrain it to a given size, and center it vertically and horizontally
+  doc.image(`QRimage/${ticketId}.png`, {
+      fit: [400, 400],
+      align: 'center',
+      valign: 'center'
+  });
+  doc.end();
+}
 
 module.exports = router
